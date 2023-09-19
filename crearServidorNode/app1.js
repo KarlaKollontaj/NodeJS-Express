6ac41f89ad82499f69a73e4da088b24e4ed62553
Karla Kollontaj <carlavaleriabarnab@gmail.com>
2023-09-19 12:18:11 +0200

    ok

--- crearServidorNode/app1.js
+++ crearServidorNode/app1.js
@@ -1,10 +1,26 @@
 const http = require('http');
 
+//info sobre req
+// const servidor = http.createServer((req, res) => {
+//     console.log('===> req(solicitud)');
+//     console.log(req.url);
+//     console.log(req.method);
+//     console.log(req.headers);
+//     res.end('Buenos dias a todes!');
+// });
+
+//info sobre res
 const servidor = http.createServer((req, res) => {
-    console.log('===> req(solicitud)');
-    console.log(req.url);
-    console.log(req.method);
-    res.end('Buenos dias a todes!');
+    console.log('===> res (respuesta)');
+    //para ver el codigo de estado
+    //console.log(res.statusCode);
+    //para cambiar el codigo de estado
+    //res.statusCode = 404;
+    //console.log(res.statusCode);
+    //para cambiar el header con info adicional
+    res.setHeader('content-type', 'application/json');
+    console.log(res.getHeaders());
+    res.end('Buenos dias a todes!')
 });
 
 const puerto = 3000;
