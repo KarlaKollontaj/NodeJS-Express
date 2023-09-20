e2ce768f819975fd6737f624f109b7cc4206d95f
Karla Kollontaj <carlavaleriabarnab@gmail.com>
2023-09-20 11:32:48 +0200

    ok

--- nodemonDemo/app.js
+++ nodemonDemo/app.js
@@ -1,12 +1,12 @@
 const http = require('http');
 
 const servidor = http.createServer((req, res) => {
-    res.end('Hola, mundo');
+    res.end('Estoy aprendiendo Node.js');
 });
 
 const puerto = 3000;
 
 servidor.listen(puerto, () => {
-    console.log(`El servidor esta escuchando en el puerto ${puerto}`)
+    console.log(`El servidor esta escuchando en el puerto ${puerto}`);
 }); 
 
