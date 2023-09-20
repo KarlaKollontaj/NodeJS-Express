d297ac3e4ef938d9bed5672660258d33580c69b6
Karla Kollontaj <carlavaleriabarnab@gmail.com>
2023-09-20 12:25:59 +0200

    ok

--- Express/app.js
+++ Express/app.js
@@ -1,2 +1,30 @@
 //la primera linea siempre importa express
 
+const express = require('express');
+
+//la segunda linea siempre define la app como la funcion express
+
+const app = express();
+
+//importamos el file cursos.js
+
+const {infoCursos} = require('./cursos.js');
+
+//IMPLEMENTAMOS LAS RUTAS (routing o en espanol enrutamiento o direccionamiento)
+//tenemos que especificar el metodo y el camino y una funcion flecha 
+
+app.get('/', (req, res) => {
+    res.send('Mi primer servidor con Express.')
+});
+
+//especificar el puerto
+//o el puerto 3000 o el puerto asignando por el ambiente de trabajo (hosting por ejemplo)
+//donde estamos creando el servidor
+
+const PUERTO = process.env.PORT || 3000;
+
+//para que el servidor escuche
+
+app.listen(PUERTO, () => {
+    console.log(`El servidor esta escuchando en el puerto ${PUERTO}...`);
+});
---
+++ Express/cursos.js
@@ -0,0 +1,43 @@
+let infoCursos = {
+    'programacion': [
+        {
+            id: 1,
+            titolo: 'Aprende Python',
+            tema: 'python',
+            vistas: 15000,
+            nivel: 'basico'
+        },
+        {
+            id: 2,
+            titolo: 'Aprende Python',
+            tema: 'python',
+            vistas: 13553,
+            nivel: 'intermedio'
+        },
+        {
+            id: 3,
+            titolo: 'Aprende Javascript',
+            tema: 'javascript',
+            vistas: 102223,
+            nivel: 'basico'
+        }
+    ],
+    'matematicas': [
+        {
+            id: 1,
+            titolo: 'Aprende Calculo',
+            tema: 'calculo',
+            vistas: 12427,
+            nivel: 'basico'
+        },
+        {
+            id: 2,
+            titolo: 'Aprende Algebra',
+            tema: 'algebra',
+            vistas: 15722,
+            nivel: 'intermedio'
+        }
+    ]
+}
+
+module.exports.infoCursos = infoCursos;
